ce0c9446df0a846af5e279c231596964c69cceba
adibw99 <50860243+adibw99@users.noreply.github.com>
2022-11-02 18:43:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import paho.mqtt.client as mqtt
 import time
 mqttc=mqtt.Client()
 mqttc.connect("broker.emqx.io", 8083, 60)                                  # Mqtt broker
-ngrok.set_auth_token("1g64Fo2JUcdnAFEj5IYVHpFrw9q_4CVPj2tEu1m2qewp3b8E4")      # Enter the registered Auth_Token
-public_url = ngrok.connect("192.168.137.139")                       # tunnel to host:port instead of localhost
+ngrok.set_auth_token("**")      # Enter the registered Auth_Token
+public_url = ngrok.connect("**")                       # tunnel to host:port instead of localhost
 
 print(public_url)                                                              # Displaying the ngrok_tunnel url 
 
